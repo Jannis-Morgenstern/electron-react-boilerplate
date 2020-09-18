5595ee7935f316741773c1ab576e32da310c33c5
Jannis-Morgenstern <jannis.morgenstern@web.de>
2020-09-18 12:53:49 +0200

    cleaned up

--- public/Main.js
+++ public/Main.js
@@ -3,14 +3,15 @@ const path = require('path');
 const fs = require('fs');
 const url = require('url');
 
+let mainWindow;
+
+let dev = false;
+
+if (process.env.NODE_ENV === 'development') {
+	dev = true;
+}
 const createWindow = () => {
-	const win = new BrowserWindow({ width: 800, height: 600 });
-
-	let dev = false;
-
-	if (process.env.NODE_ENV === 'development') {
-		dev = true;
-	}
+	mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
 	let indexPath;
 
@@ -29,7 +30,30 @@ const createWindow = () => {
 		});
 	}
 
-	win.loadURL(indexPath);
+	mainWindow.loadURL(indexPath);
+
+	mainWindow.once('ready-to-show', () => {
+		mainWindow.show();
+	});
+
+	mainWindow.on('closed', () => (mainWindow = null));
 };
 
 app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
