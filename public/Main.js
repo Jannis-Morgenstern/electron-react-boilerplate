b329a8b5e906063176ba71de40f0044e3e0424d3
Jannis-Morgenstern <jannis.morgenstern@web.de>
2020-09-18 10:29:39 +0200

    added dev env

--- public/Main.js
+++ public/Main.js
@@ -1,10 +1,35 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const fs = require('fs');
+const url = require('url');
 
 const createWindow = () => {
 	const win = new BrowserWindow({ width: 800, height: 600 });
 
-	win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
+	let dev = false;
+
+	if (process.env.NODE_ENV === 'development') {
+		dev = true;
+	}
+
+	let indexPath;
+
+	if (dev) {
+		indexPath = url.format({
+			protocol: 'http:',
+			host: 'localhost:3000',
+			pathname: 'index.html',
+			slashes: true,
+		});
+	} else {
+		indexPath = url.format({
+			protocol: 'file:',
+			host: path.join(__dirname, '../build/index.html'),
+			slashes: true,
+		});
+	}
+
+	win.loadURL(indexPath);
 };
 
 app.on('ready', createWindow);
